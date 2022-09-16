0f497e547be7ae239677806e9b405ca167b8757c
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-09-16 09:32:51 -0500

    added user, userbook scaffold

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+    has_many :users, through: :user_books
+    has_many :user_books
+
+    has_many :users, through: :userbooknews
+    has_many :userbooknews
+
     validates :title, presence: true
     validates :author, presence: true
     validates :price, presence: true
