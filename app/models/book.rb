01cb015fefefd041efbce12c41fc3785be07218b
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-09-19 22:11:49 -0500

    active record updates

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,6 @@ class Book < ApplicationRecord
     has_many :users, through: :user_books
     has_many :user_books
 
-    has_many :users, through: :userbooknews
-    has_many :userbooknews
 
     validates :title, presence: true
     validates :author, presence: true
